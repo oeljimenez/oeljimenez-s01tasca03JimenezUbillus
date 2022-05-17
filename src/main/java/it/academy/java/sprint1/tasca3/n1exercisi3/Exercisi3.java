dfd786579878642a586dca54f156e4b9a6ccb5ac
leonardo-javier.jimenez <extern.jimenez-ubillus_leonardo-javier@allianz.com>
2022-05-17 12:38:59 +0200

    fixing exercices order

--- src/main/java/it/academy/java/sprint1/tasca3/n1exercisi3/Exercisi3.java
+++ src/main/java/it/academy/java/sprint1/tasca3/n1exercisi3/Exercisi3.java
@@ -10,22 +10,5 @@ public class Exercisi3 {
     public static void main(String[] args) {
         System.out.println("Tasca 03. Java Collections (Spring1), Nivell 1, Exercisi 3");
 
-        //creating first list
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i < 11 ; i++) {
-            list.add(i);
-        }
-        System.out.println(list.toString());
-
-        // Generate an iterator to iterate List in reverse
-        ListIterator<Integer> gfg_itr
-                = list.listIterator(list.size());
-        //new list creation
-        List<Integer> list2 = new ArrayList<>();
-        while (gfg_itr.hasPrevious())  {
-            // Iterate in reverse
-            list2.add(gfg_itr.previous());
-        }
-        System.out.println(list2.toString());
     }
 }
